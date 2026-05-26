df2b2ae5e899485844fd30e31836aaaca209012f
Deepak Kumar <deepakkr62040@gmail.com>
2026-05-26 22:52:27 +0530

    add file and program of datatype

---
+++ Java/dataType/DataDemo.java
@@ -0,0 +1,15 @@
+package Java.dataType;
+
+public class DataDemo {
+    public static void main(String args[]){
+        int a = 10;
+        char c = 'D';
+        float price = 5;
+        boolean d = true;
+
+        System.out.println("int data a : " + a);
+        System.out.println("char data c : " + c);
+        System.out.println("float value price : " + price);
+        System.out.println("boolean value : " + d);
+    }
+}
