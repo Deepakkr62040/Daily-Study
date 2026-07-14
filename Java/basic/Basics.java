372a8222fbf035ed036a4e813b3b8afd213e25f8
Deepak Kumar <149265493+Deepakkr62040@users.noreply.github.com>
2026-07-15 00:36:38 +0530

    Implement JDBC connection
    Added a new class for database connectivity using JDBC.

--- Java/basic/Basics.java
+++ Java/basic/Basics.java
@@ -5,3 +5,40 @@ public class Basics {
         System.out.println("Hello, Deepak");
     }
 }
+
+
+//advance java database connectivity................
+package myfirstapplication;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class MyFirstApplication {
+    public static void main(String[] args) {
+        Connection conn = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            String url = "jdbc:mysql://localhost:3306/advanceJava";
+            String user = "root";
+            String password = "";
+            
+            conn = DriverManager.getConnection(url, user, password);
+            
+            System.out.println("Database connection successfullly.");
+        } catch(ClassNotFoundException e){
+            System.out.println("Error: JDBC driver not found.");
+        } catch(SQLException e){
+            System.out.println("Error: Database connection failed.");
+            System.out.println("Reason: " + e.getMessage());
+        } finally {
+            try{
+                if(conn != null)
+                    conn.close();
+            } catch(SQLException e){
+                System.out.println("Error: while closing connection");
+            }
+        }
+    }
+}
